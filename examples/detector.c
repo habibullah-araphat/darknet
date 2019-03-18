42f84d73b0d933e866569a58e84365f0bd9fed7a
konok <habibullah.araphat@gmail.com>
2019-03-18 18:47:13 +0600

    continue added for read_cnt<0

--- examples/detector.c
+++ examples/detector.c
@@ -68,7 +68,8 @@ void* receive_message_thread(void *inp){
 	while(should_continue_training == 1){
 		/*Receive the message from server*/
 		int read_cnt = recv((socketDescriptor),recv_buffer,sizeof(recv_buffer),0);
-		if(read_cnt<=0){
+		if(read_cnt<0)continue;
+        if(read_cnt==0){
             should_continue_training = 0;
 			printf("server disconnected: read_cnt:%d\n", read_cnt);
 			break;
